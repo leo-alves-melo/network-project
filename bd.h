606b0f6130bfe6b3e9820a305dfe8b054911a6d1
Mateus Coelho <mateus_coelho@live.com>
2018-04-11 02:57:35 -0300

    atualizada documentacao

--- bd.h
+++ bd.h
@@ -1,4 +1,14 @@
-/*data base utilits*/
+/*data base utilities*/
+
+/* ------------------ README --------------------------
+   you MUST only use the following functions:
+   - initialize
+   - make_client_message
+   - parse_client_message
+   - parse_server_message
+
+   the remaining function should not be called because they are internally
+   by the exposed functions. */
 
 /* initialize the data structure (json)
    given a file name, it returns the file pointer and the json tree structure
@@ -7,10 +17,11 @@ int initialize(char* file_name, FILE** fp, cJSON** json);
 
 /* fetch some data in the json structure stored in the db. the data that is
    going to be fetched depends on the type of message that the server will send.
-   given the message number and the db, returns a json structure with an object
-   as root similar to the default structure of the bd.  */
+   given the message number, the db and maybe a course code, it returns a json
+   structure with an object as root similar to the default structure of the bd. */
 cJSON* fetch_info(int mes_numb, cJSON* db, char* code);
 
+/*Replace the class comment.  */
 void write_comment(cJSON* db, char* code, char* comment);
 
 /* make a server message with the message number and the content fetched in
@@ -19,20 +30,25 @@ void write_comment(cJSON* db, char* code, char* comment);
    representation of the message. */
 char* make_server_message(int mes_numb, cJSON* db);
 
-/* make a client message with the message number and content, that can be
-   a code and/or a comment
+/* make a client message with the message number and content. the latter is an
+   object that can have course code and/or class comment.
    given the messaage number, the code and the comment, it returns the string
-   representation of the message */
+   representation of the message
+   note: this function behavior is determined by its message number, so for
+   example if a message has number 1, it doesn't require neither code nor comment.
+   it means that it is possible to put anything in these params and the function
+   will still works. but to avoid problems it is highly recommend to put NULLs
+   in these fields if will not use them. */
 char* make_client_message(int mes_numb, char* code, char* comment);
 
 /* parse a server message received by the client and prints in the screen
    the data requested.
-   given the message number, it returns an integer indicating if the function
+   given the message , it returns an integer indicating if the function
    worked properly */
 int parse_server_message(char* message);
 
-/* parse a client message received by the server and returns a message that is
-   contains the requested data
-   given the message number and a pointer to the message that will be sent,
-   it returns an integer indicating if the function worked properly  */
+/* parse a client message received by the server and returns a message that
+   contains the requested data that will be sent back to the client.
+   given the message and the db, it returns an integer indicating if the
+   function worked properly and a pointer to the next message  */
 int parse_client_message(char* message, char** nxt_message, cJSON* db);
